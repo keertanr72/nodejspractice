00aa6fa7456819a3192fe2c102d08c884459f409
Keertan R <keertanr72@gmail.com>
2023-02-14 01:28:39 +0530

    commit7

--- nodepractice.js
+++ nodepractice.js
@@ -4,6 +4,31 @@
 
 const http = require('http')
 const server = http.createServer((req, res) => {
-    console.log('Keertan R')
+    console.log(req.url, req.method, req.headers)
+    //process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    // res.write('<html>')
+    // res.write('<head><title>Node title</title></head>')
+    // res.write('<body>Hello there</body>')
+    // res.write('</html>')
+    if(req.url == '/home'){
+    res.write('<html>')
+    res.write('<head><title>Node title</title></head>')
+    res.write('<body>Welcome Home</body>')
+    res.write('</html>')
+    }
+    if(req.url == '/about'){
+        res.write('<html>')
+        res.write('<head><title>Node title</title></head>')
+        res.write('<body>Welcome to About Us page</body>')
+        res.write('</html>')
+        }
+    if(req.url == '/node'){
+    res.write('<html>')
+    res.write('<head><title>Node title</title></head>')
+    res.write('<body>Welcome to my Node Js project</body>')
+    res.write('</html>')
+    }
+    res.end()
 })
 server.listen(4000)
