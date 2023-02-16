b3262a6b230bbb3618a57938b3bc2e3a4e92d4b0
Keertan R <keertanr72@gmail.com>
2023-02-16 13:33:20 +0530

    commit9

--- nodepractice.js
+++ nodepractice.js
@@ -3,35 +3,6 @@
 // fs.writeFileSync('hello.txt','hello from node.js')
 
 const http = require('http')
-const fs = require('fs')
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    if(req.url === '/'){
-        fs.readFile('message.txt', {encoding: 'utf-8'}, (err, data) => {
-            console.log(data);
-            res.write('<html>')
-            res.write('<head><title>Node title</title></head>')
-            res.write(`<body>${data}</body>`)
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
-            res.write('</html>')
-            return res.end()
-        })
-    }
-    else if(req.url === '/message' && req.method === 'POST'){
-        const body = []
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk)
-        })
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1]
-            fs.writeFile('message.txt',message, (err) => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
-        })
-    }
-})
+const routes = require('./routes')
+const server = http.createServer(routes)
 server.listen(4000)
