75c4d936e5c5d80a4e575f125a0be6bce79bcfd6
Keertan R <keertanr72@gmail.com>
2023-02-17 19:21:42 +0530

    express3

--- nodepractice.js
+++ nodepractice.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-app.use((req, res, next) => {
-    console.log('in the middleware')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><label>Item</label><input typr="text" name="title"><br><label>Size</label><input typr="text" name="size"><br><button type="submit">Submit</button></form>')
     next()
 })
+app.use('/product',(req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
 app.use((req, res, next) => {
-    console.log('in the middleware')
     res.send('<h1>Hello hello</h1>')
 })
 app.listen(4000)
