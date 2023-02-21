03dc5397778a56af2701ae5455337d69af45a16e
Keertan R <keertanr72@gmail.com>
2023-02-21 13:11:44 +0530

    committingmiddlewarewebpage

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
-router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/shops/product" method="POST"><label>Item</label><input typr="text" name="title"><br><label>Size</label><input typr="text" name="size"><br><button type="submit">Submit</button></form>')
-    next()
+router.get('/admin',(req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 router.post('/product',(req, res, next) => {
-    console.log(req.body)
+    console.log(req.body.title)
     res.redirect('/shops')
 })
 module.exports = router
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,8 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 router.get('/',(req, res, next) => {
-    res.send('<h1>Hello hello</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+    console.log('in shop')
 })
 module.exports = router
