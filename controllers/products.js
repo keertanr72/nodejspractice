95b2dd82e2df5bbbf686e1c5547ed150ec76abd3
Keertan R <keertanr72@gmail.com>
2023-02-25 17:45:39 +0530

    commit8th

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,17 @@
-const products = []
+const Product = require('../models/product')
 const path = require('path')
 exports.getAddProduct = (req, res, next) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 }
 exports.postAddProducts = (req, res, next) => {
     console.log(req.body.title)
-    products.push({title: req.body.title})
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/shops')
 }
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll(() => {
     res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
     console.log('in shop')
+    })
 }
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const productsController = require('../controllers/products')
-const { getProducts } = require('../controllers/products')
+const getProducts = require('../controllers/products')
 const router = express.Router()
 router.get('/',productsController.getProducts)
 module.exports = router
